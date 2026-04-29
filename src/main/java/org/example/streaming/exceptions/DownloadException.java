c11625b00c0c4bfe9fe208120e6b4bdae7fbabe1
Gabriel <gabrielsantostca@gmail.com>
2026-04-28 23:25:25 -0300

    feat: new Download exception

---
+++ src/main/java/org/example/streaming/exceptions/DownloadException.java
@@ -0,0 +1,8 @@
+package org.example.streaming.exceptions;
+
+public class DownloadException extends RuntimeException {
+
+    public DownloadException(String message) {
+        super(message);
+    }
+}
