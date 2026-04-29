11d5a4dfccbd105182b9ebf3b94c479a6a25d31f
Gabriel <gabrielsantostca@gmail.com>
2026-04-28 23:25:58 -0300

    feat: implemented new download from local file source

--- src/main/java/org/example/streaming/services/FileService.java
+++ src/main/java/org/example/streaming/services/FileService.java
@@ -1,8 +1,16 @@
 package org.example.streaming.services;
 
+import org.example.streaming.entity.dto.DownloadDto;
+import org.example.streaming.entity.dto.FilesDto;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface FileService {
 
     void upload(MultipartFile document);
+
+    FilesDto listFiles();
+
+    DownloadDto download(String fileIdentifier) throws IOException;
 }
--- src/main/java/org/example/streaming/services/impl/LocalFileService.java
+++ src/main/java/org/example/streaming/services/impl/LocalFileService.java
@@ -1,19 +1,27 @@
 package org.example.streaming.services.impl;
 
+import org.example.streaming.entity.dto.DownloadDto;
+import org.example.streaming.entity.dto.FilesDto;
+import org.example.streaming.exceptions.DownloadException;
 import org.example.streaming.exceptions.UploadException;
 import org.example.streaming.services.FileService;
 import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedOutputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class LocalFileService implements FileService {
@@ -32,13 +40,47 @@ public class LocalFileService implements FileService {
         }
 
         try {
-            OutputStream out = new FileOutputStream(buildPath(document.getOriginalFilename()));
+            OutputStream out = new BufferedOutputStream(new FileOutputStream(buildPath(document.getOriginalFilename())), 16384);
             document.getInputStream().transferTo(out);
         } catch (FileNotFoundException e) {
             throw new UploadException("File is empty or broken");
         } catch (IOException e) {
             throw new UploadException("Failed to write file");
         }
+    }
+
+    @Override
+    public FilesDto listFiles() {
+        Path path = Paths.get(getDestinationFolder());
+        if (!Files.isDirectory(path)){
+            return null;
+        }
+        List<String> fileNames = new ArrayList<>();
+        try (Stream<Path> paths = Files.list(path)){
+            paths.forEach(file -> fileNames.add(file.getFileName().toString()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new FilesDto(fileNames);
+    }
+
+    @Override
+    public DownloadDto download(String fileIdentifier) {
+        Path path = Paths.get(getDestinationFolder(), fileIdentifier);
+        if (!Files.exists(path)){
+            throw new DownloadException("File not found");
+        }
+
+        try {
+            InputStream stream = Files.newInputStream(path);
+            long size = Files.size(path);
+            return new DownloadDto(stream, size);
+        } catch (IOException e) {
+            throw new DownloadException("Internal server error while downloading file");
+        }
+
+
     }
 
     private void createFolder(Path path) {
